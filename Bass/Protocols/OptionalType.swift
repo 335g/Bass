603ddd7bd78879e9816dc80cb74396bbfd27e96d
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-05-13 21:53:59 +0900

    Add map/flatMap (OptionalType)

--- Bass/Protocols/OptionalType.swift
+++ Bass/Protocols/OptionalType.swift
@@ -14,3 +14,15 @@ extension Optional: OptionalType {
 	}
 }
 
+// MARK: - OptionalType - map/flatMap
+
+public extension OptionalType {
+	public func map<A>(@noescape f: Wrapped throws -> A) rethrows -> A? {
+		return try optional.map(f)
+	}
+	
+	public func flatMap<A>(@noescape f: Wrapped throws -> A?) rethrows -> A? {
+		return try optional.flatMap(f)
+	}
+}
+
