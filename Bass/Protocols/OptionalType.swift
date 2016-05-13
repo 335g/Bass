45e07b3d83a7f528107d401b5ff6624076ff65a9
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-05-13 21:49:07 +0900

    Add OptionalType

---
+++ Bass/Protocols/OptionalType.swift
@@ -0,0 +1,16 @@
+//  Copyright © 2016 Yoshiki Kudo. All rights reserved.
+
+// MARK: - OptionalType
+
+public protocol OptionalType {
+	associatedtype Wrapped
+	
+	var optional: Wrapped? { get }
+}
+
+extension Optional: OptionalType {
+	public var optional: Wrapped? {
+		return self
+	}
+}
+
