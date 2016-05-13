c7b7d5c65b5c2ea6a91e846ab1b72e0738521f8b
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-05-14 03:27:50 +0900

    Add map/flatMap/ap (IdentityType)

--- Bass/Types/Identity.swift
+++ Bass/Types/Identity.swift
@@ -72,3 +72,67 @@ public final class Identity<T>: IdentityType {
 		self.value = value
 	}
 }
+
+// MARK: - IdentityType - map/flatMap
+
+public extension IdentityType {
+	public func map<U>(f: Value -> U) -> Identity<U> {
+		return Identity(f(self.value))
+	}
+	
+	public func flatMap<U>(f: Value -> Identity<U>) -> Identity<U> {
+		return f(self.value)
+	}
+}
+
+public func <^> <U, IT: IdentityType>(f: IT.Value -> U, id: IT) -> Identity<U> {
+	return id.map(f)
+}
+
+public func >>- <U, IT: IdentityType>(id: IT, f: IT.Value -> Identity<U>) -> Identity<U> {
+	return id.flatMap(f)
+}
+
+// MARK: - IdentityType (Value: OptionalType) - map/flatMap
+
+public extension IdentityType where Value: OptionalType {
+	public func map<U>(f: Value.Wrapped -> U) -> Identity<U?> {
+		return Identity(f <^> self.value)
+	}
+	
+	public func flatMap<U>(f: Value.Wrapped -> Identity<U>) -> Identity<U?> {
+		return Identity( (self.value >>- f)?.value )
+	}
+}
+
+public func <^> <U, IT: IdentityType where IT.Value: OptionalType>(f: IT.Value.Wrapped -> U, id: IT) -> Identity<U?> {
+	return id.map(f)
+}
+
+public func >>- <U, IT: IdentityType where IT.Value: OptionalType>(id: IT, f: IT.Value.Wrapped -> Identity<U>) -> Identity<U?> {
+	return id.flatMap(f)
+}
+
+// MARK: - IdentityType - ap
+
+public extension IdentityType {
+	public func ap<T, IT: IdentityType where IT.Value == Value -> T>(id: IT) -> Identity<T> {
+		return .pure(id.value(self.value))
+	}
+}
+
+public func <*> <T, U, IT1: IdentityType, IT2: IdentityType where IT1.Value == T -> U, IT2.Value == T>(left: IT1, right: IT2) -> Identity<U> {
+	return right.ap(left)
+}
+
+// MARK: - IdentityType (Value: OptionalType) - ap
+
+public extension IdentityType where Value: OptionalType {
+	public func ap<T, IT: IdentityType where IT.Value == Value.Wrapped -> T>(id: IT) -> Identity<T?> {
+		return .pure(id.value <^> self.value)
+	}
+}
+
+public func <*> <T: OptionalType, U, IT1: IdentityType, IT2: IdentityType where IT1.Value == T.Wrapped -> U, IT2.Value == T>(left: IT1, right: IT2) -> Identity<U?> {
+	return right.ap(left)
+}
