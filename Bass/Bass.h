abd5aaf66b170e6eca5b0c518ef0d5b608ef2529
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-05-13 21:13:59 +0900

    First commit

---
+++ Bass/Bass.h
@@ -0,0 +1,19 @@
+//
+//  Bass.h
+//  Bass
+//
+//  Created by Yoshiki Kudo on 2016/05/13.
+//  Copyright © 2016年 Yoshiki Kudo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Bass.
+FOUNDATION_EXPORT double BassVersionNumber;
+
+//! Project version string for Bass.
+FOUNDATION_EXPORT const unsigned char BassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bass/PublicHeader.h>
+
+
