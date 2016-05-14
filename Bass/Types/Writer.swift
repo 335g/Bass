8b28c553ae797a450da6cf787c4ac3efde125edc
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-05-14 15:52:12 +0900

    Add map/flatMap (WriterType)

--- Bass/Types/Writer.swift
+++ Bass/Types/Writer.swift
@@ -51,6 +51,80 @@ public extension WriterType where ValuesW == (ResultW, OutputW) {
 	}
 }
 
+public extension WriterType where ValuesW == ((ResultW, OutputW -> OutputW), OutputW) {
+	/// `pass` is an action that executes the action, which returns
+	/// a value and a function, and return the value, applying the function to the output.
+	public func pass() -> Writer<OutputW, ResultW, (ResultW, OutputW)> {
+		let ((r, f), o) = run.value
+		return Writer( Identity(r, f(o)) )
+	}
+}
+
+// MARK: - WriterType - map/flatMap
+
+public extension WriterType where ValuesW == (ResultW, OutputW) {
+	public func map<Result2, Output2: Monoid>(f: (ResultW, OutputW) -> (Result2, Output2)) -> Writer<Output2, Result2, (Result2, Output2)> {
+		return Writer(f <^> run)
+	}
+	
+	public func map<Result2>(f: ResultW -> Result2) -> Writer<OutputW, Result2, (Result2, OutputW)> {
+		return map { (f($0), $1) }
+	}
+	
+	public func flatMap<Result2>(g: ResultW -> Writer<OutputW, Result2, (Result2, OutputW)>) -> Writer<OutputW, Result2, (Result2, OutputW)> {
+		let f2: (ResultW, OutputW) -> (Result2, OutputW) = { r, o in
+			let (r2, o2) = g(r).run.value
+			return (r2, o.mappend(o2))
+		}
+		
+		let (r, o) = run.value
+		let (r2, o2) = f2(r, o)
+		
+		return Writer( .pure(r2, o2) )
+	}
+}
+
+/// Alias for `map(f:)`
+public func <^> <M: Monoid, T1, T2, WT: WriterType where WT.OutputW == M, WT.ResultW == T1, WT.ValuesW == (T1, M)>(f: T1 -> T2, g: WT) -> Writer<M, T2, (T2, M)> {
+	return g.map(f)
+}
+
+/// Alias for `flatMap(g:)`
+public func >>- <M: Monoid, T1, T2, WT: WriterType where WT.OutputW == M, WT.ResultW == T1, WT.ValuesW == (T1, M)>(m: WT, fn: T1 -> Writer<M, T2, (T2, M)>) -> Writer<M, T2, (T2, M)> {
+	return m.flatMap(fn)
+}
+
+// MARK: - WriterType (Values: OptionalType) - map/flatMap
+
+public extension WriterType where ValuesW == (ResultW, OutputW)? {
+	public func map<Result2, Output2: Monoid>(f: (ResultW, OutputW) -> (Result2, Output2)) -> Writer<Output2, Result2, (Result2, Output2)?> {
+		return Writer(f <^> run)
+	}
+	
+	public func map<Result2>(f: ResultW -> Result2) -> Writer<OutputW, Result2, (Result2, OutputW)?> {
+		return map { (f($0), $1) }
+	}
+	
+	public func flatMap<Result2>(g: ResultW -> Writer<OutputW, Result2, (Result2, OutputW)>) -> Writer<OutputW, Result2, (Result2, OutputW)?> {
+		let f2: (ResultW, OutputW) -> (Result2, OutputW) = { r, o in
+			let (r2, o2) = g(r).run.value
+			return (r2, o.mappend(o2))
+		}
+		
+		return Writer( .pure(f2 <^> run.value) )
+	}
+}
+
+/// Alias for `map(f:)`
+public func <^> <M: Monoid, T1, T2, WT: WriterType where WT.OutputW == M, WT.ResultW == T1, WT.ValuesW == (T1, M)?>(f: T1 -> T2, g: WT) -> Writer<M, T2, (T2, M)?> {
+	return g.map(f)
+}
+
+/// Alias for `flatMap(g:)`
+public func >>- <M: Monoid, T1, T2, WT: WriterType where WT.OutputW == M, WT.ResultW == T1, WT.ValuesW == (T1, M)?>(m: WT, fn: T1 -> Writer<M, T2, (T2, M)>) -> Writer<M, T2, (T2, M)?> {
+	return m.flatMap(fn)
+}
+
 // MARK: - Writer
 
 // TODO: re-define Writer (Swift3)
