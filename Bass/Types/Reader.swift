ef8f649696e0223ea95551c12666a1fed7098952
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-05-14 10:20:42 +0900

    Reader conform to ReaderType

--- Bass/Types/Reader.swift
+++ Bass/Types/Reader.swift
@@ -8,11 +8,25 @@ public protocol ReaderType {
 	
 	var run: EnvR -> ValueR { get }
 	init(_ run: EnvR -> ValueR)
+	
+	var reader: Reader<EnvR, ValueR> { get }
 }
 
 // MARK: - Reader
 
 public struct Reader<R, A> {
-	let run: R -> A
+	public let run: R -> A
+}
+
+// MARK: - Reader: ReaderType
+
+extension Reader: ReaderType {
+	public init(_ run: R -> A) {
+		self.run = run
+	}
+	
+	public var reader: Reader<R, A> {
+		return self
+	}
 }
 
