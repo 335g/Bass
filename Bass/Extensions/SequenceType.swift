434d71ea214db4979e1a0df0dd0d0092dff5812c
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-05-14 17:52:39 +0900

    Add sequence (SequenceType where Generator.Element: StateType)

--- Bass/Extensions/SequenceType.swift
+++ Bass/Extensions/SequenceType.swift
@@ -5,7 +5,7 @@
 public extension SequenceType where Generator.Element: ReaderType {
 	public func sequece() -> Reader<Generator.Element.EnvR, [Generator.Element.ValueR]> {
 		return reduce(.pure([])){ acc, elem in
-			return acc >>- { xs in elem >>- { .pure(xs + [$0]) } }
+			acc >>- { xs in elem >>- { .pure(xs + [$0]) } }
 		}
 	}
 }
@@ -13,7 +13,15 @@ public extension SequenceType where Generator.Element: ReaderType {
 public extension SequenceType where Generator.Element: WriterType, Generator.Element.ValuesW == (Generator.Element.ResultW, Generator.Element.OutputW) {
 	public func sequence() -> Writer<Generator.Element.OutputW, [Generator.Element.ResultW], ([Generator.Element.ResultW], Generator.Element.OutputW)> {
 		return reduce(.pure([])){ acc, elem in
-			return acc >>- { xs in elem >>- { .pure(xs + [$0]) } }
+			acc >>- { xs in elem >>- { .pure(xs + [$0]) } }
+		}
+	}
+}
+
+public extension SequenceType where Generator.Element: StateType, Generator.Element.ValuesS == (Generator.Element.ResultS, Generator.Element.StateS) {
+	public func sequence() -> State<Generator.Element.StateS, [Generator.Element.ResultS], ([Generator.Element.ResultS], Generator.Element.StateS)> {
+		return reduce(.pure([])){ acc, elem in
+			acc >>- { xs in elem >>- { .pure(xs + [$0]) } }
 		}
 	}
 }
