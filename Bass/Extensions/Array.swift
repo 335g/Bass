2d12d7d95d16a9684a3dedcaf702efc9417e3b20
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-05-15 00:02:22 +0900

    Array conform to Foldable

--- Bass/Extensions/Array.swift
+++ Bass/Extensions/Array.swift
@@ -11,3 +11,33 @@ extension Array: Monoid {
 		return []
 	}
 }
+
+extension Array: Foldable {
+	public func foldr<T>(initial: T, _ f: Element -> T -> T) -> T {
+		return reverse().reduce(initial){ f($1)($0) }
+	}
+	
+	public func foldl<T>(initial: T, _ f: T -> Element -> T) -> T {
+		return reduce(initial, combine: uncurry(f))
+	}
+	
+	public func null() -> Bool {
+		return isEmpty
+	}
+	
+	public func length() -> Int {
+		return count
+	}
+	
+	public func find(predicate: Element -> Bool) throws -> Element? {
+		guard let index = indexOf(predicate) else {
+			return nil
+		}
+		
+		return self[index]
+	}
+	
+	public func toList() -> [Element] {
+		return self
+	}
+}
