a183070928c60b1336610e0a4e1ef1f2efc9168a
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-05-14 16:12:12 +0900

    Add sequence (SequenceType where Generator.Element: WriterType)

--- Bass/Extensions/SequenceType.swift
+++ Bass/Extensions/SequenceType.swift
@@ -9,3 +9,11 @@ public extension SequenceType where Generator.Element: ReaderType {
 		}
 	}
 }
+
+public extension SequenceType where Generator.Element: WriterType, Generator.Element.ValuesW == (Generator.Element.ResultW, Generator.Element.OutputW) {
+	public func sequence() -> Writer<Generator.Element.OutputW, [Generator.Element.ResultW], ([Generator.Element.ResultW], Generator.Element.OutputW)> {
+		return reduce(.pure([])){ acc, elem in
+			return acc >>- { xs in elem >>- { .pure(xs + [$0]) } }
+		}
+	}
+}
