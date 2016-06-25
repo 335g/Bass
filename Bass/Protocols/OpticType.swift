f1e166189097aade753f6b7253254622b1065278
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-06-25 20:40:47 +0900

    Add OpticType

---
+++ Bass/Protocols/OpticType.swift
@@ -0,0 +1,10 @@
+//  Copyright © 2016 Yoshiki Kudo. All rights reserved.
+
+// MARK: - OpticType
+
+public protocol OpticType {
+	associatedtype Source
+	associatedtype Target
+	associatedtype AltSource
+	associatedtype AltTarget
+}
