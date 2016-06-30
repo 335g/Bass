93d83641bea854616d1fb596d7e33e2b87114496
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-06-30 21:09:16 +0900

    Int conform some protocol

---
+++ Bass/Extensions/Int.swift
@@ -0,0 +1,19 @@
+//  Copyright © 2016 Yoshiki Kudo. All rights reserved.
+
+extension Int: Semigroup {
+	public func mappend(_ x: Int) -> Int {
+		return self + x
+	}
+}
+
+extension Int: Monoid {
+	public static var mempty: Int {
+		return 0
+	}
+}
+
+extension Int: Foldable {
+	public func foldr<T>(initial: T, f: (Int) -> (T) -> T) -> T {
+		return f(self)(initial)
+	}
+}
